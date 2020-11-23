71064816a08542ada94b1e6bba6a952126964c77
Clems-web <clement.servais1@gmail.com>
2020-11-23 13:45:52 +0100

    finito

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let result = 5 + 6;
 let addition = result + 2;
 let soustraction = result - 1;
 
-alert(result * addition)
-alert (result ** soustraction)
-alert(5 % 2)
+alert(result * addition);
+alert (result ** soustraction);
+alert(5 % 2);
